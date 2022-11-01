f8483558678c58c746ef1babefb84de0fa9ef2c6
wisezeee <gafiatullinakarina26@gmail.com>
2022-11-01 09:32:16 +0300

    improve

--- import_func.py
+++ import_func.py
@@ -1,8 +1,8 @@
-
+"file where we import function"
 from project import readfile
 while True:
-    file = input("Выберите файл - harry_potter.txt или avengers.txt (q чтобы выйти): ")
-    if file == 'q':
+    file_inp = input("Выберите файл - harry_potter.txt или avengers.txt (q чтобы выйти): ")
+    if file_inp == 'q':
         break
     numberofstring = int(input('Введите номер от 1 до 7: '))
-    print(readfile(file, numberofstring))
+    print(readfile(file_inp, numberofstring))
